dbe2c3b2710336ea134ad69ac0cfa422adc9c7a0
Ryan Ford <ford@students.uni-marburg.de>
2022-02-03 13:47:00 +0100

    feat: dark mode in CreateUserModal, RoleSwitch, and fixes in UserManagement

--- src/components/user/RoleSwitch.tsx
+++ src/components/user/RoleSwitch.tsx
@@ -18,7 +18,7 @@ export default function RoleSwitch({
   return (
     <Switch.Group as="div" className={"flex mt-4 items-center"}>
       <Switch.Label as="span" className="w-20 mr-4">
-        <span className="text-sm mr-14 font-medium text-gray-900">{name}</span>
+        <span className="text-sm mr-14 font-medium text-gray-900 dark:text-gray-300 ">{name}</span>
       </Switch.Label>
       <Switch
         checked={checkedCond}
@@ -27,7 +27,7 @@ export default function RoleSwitch({
           else remove();
         }}
         className={classNames(
-          checkedCond ? "bg-indigo-600" : "bg-gray-200",
+          checkedCond ? "bg-indigo-600 dark:bg-indigo-700" : "bg-gray-200 dark:bg-gray-600",
           "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         )}
       >
